252821ad1e7616c1f47ddc1ac1928e6dd4a1acc1
dalin sun <sundalin0512@outlook.com>
2017-05-07 21:54:00 +0800

    80

--- EchoCancelWithCaptureAndRender/EchoCancelWithCaptureAndRender.cpp
+++ EchoCancelWithCaptureAndRender/EchoCancelWithCaptureAndRender.cpp
@@ -104,7 +104,6 @@ HRESULT RenderThread()
 {
 	HRESULT hr;
 	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
-	REFERENCE_TIME hnsActualDuration;
 	IMMDeviceEnumerator *pEnumerator = NULL;
 	IMMDevice *pDevice = NULL;
 	IAudioClient *pAudioClient = NULL;
@@ -147,10 +146,6 @@ HRESULT RenderThread()
 		NULL);
 	EXIT_ON_ERROR(hr);
 
-	// Tell the audio source which format to use.
-	//hr = pMySource->SetFormat(pwfx);
-	EXIT_ON_ERROR(hr);
-
 	// Get the actual size of the allocated buffer.
 	hr = pAudioClient->GetBufferSize(&bufferFrameCount);
 	EXIT_ON_ERROR(hr);
@@ -173,7 +168,6 @@ HRESULT RenderThread()
 	EnterCriticalSection(&criticalSection);
 	CopyMemory(pData, tmpRenderData, tmpRenderFramesNum * 8);
 	delete[] tmpRenderData;
-	bufferFrameCount = tmpRenderFramesNum;
 	LeaveCriticalSection(&criticalSection);
 
 	EXIT_ON_ERROR(hr);
@@ -181,18 +175,12 @@ HRESULT RenderThread()
 	hr = pRenderClient->ReleaseBuffer(bufferFrameCount, flags);
 	EXIT_ON_ERROR(hr);
 
-	// Calculate the actual duration of the allocated buffer.
-	hnsActualDuration = (double)REFTIMES_PER_SEC *
-		bufferFrameCount / pwfx->nSamplesPerSec;
-
 	hr = pAudioClient->Start();  // Start playing.
 	EXIT_ON_ERROR(hr);
 
 	// Each loop fills about half of the shared buffer.
 	while (flags != AUDCLNT_BUFFERFLAGS_SILENT)
 	{
-		// Sleep for half the buffer duration.
-		//Sleep((DWORD)(hnsActualDuration / REFTIMES_PER_MILLISEC / 2));
 
 		// See how much buffer space is available.
 		hr = pAudioClient->GetCurrentPadding(&numFramesPadding);
@@ -206,24 +194,20 @@ HRESULT RenderThread()
 
 		// Get next 1/2-second of data from the audio source.
 		EnterCriticalSection(&criticalSection);
-		tmpRenderFramesNum = bufferFrameCount;
+		tmpRenderFramesNum = numFramesAvailable;
 		LeaveCriticalSection(&criticalSection);
 		PulseEvent(hEvent);
 		Sleep(1);
 		EnterCriticalSection(&criticalSection);
 		CopyMemory(pData, tmpRenderData, tmpRenderFramesNum * 8);
 		delete[] tmpRenderData;
-		bufferFrameCount = tmpRenderFramesNum;
 		LeaveCriticalSection(&criticalSection);
 		EXIT_ON_ERROR(hr);
 
 		hr = pRenderClient->ReleaseBuffer(numFramesAvailable, flags);
-		Sleep(10);
+		Sleep(500);
 		EXIT_ON_ERROR(hr);
 	}
-
-	// Wait for last data in buffer to play before stopping.
-	//Sleep((DWORD)(hnsActualDuration / REFTIMES_PER_MILLISEC / 2));
 
 RenderStop:
 	hr = pAudioClient->Stop();  // Stop playing.
@@ -241,8 +225,7 @@ RenderStop:
 HRESULT CaptureThread()
 {
 	HRESULT hr;
-	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
-	REFERENCE_TIME hnsActualDuration;
+	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC/50;
 	UINT32 bufferFrameCount;
 	UINT32 numFramesAvailable;
 	IMMDeviceEnumerator *pEnumerator = NULL;
@@ -297,10 +280,6 @@ HRESULT CaptureThread()
 	// Notify the audio sink which format to use.
 	//hr = pMySink->SetFormat(pwfx);
 	EXIT_ON_ERROR(hr);
-
-	// Calculate the actual duration of the allocated buffer.
-	hnsActualDuration = (double)REFTIMES_PER_SEC *
-		bufferFrameCount / pwfx->nSamplesPerSec;
 
 	hr = pAudioClient->Start();  // Start recording.
 	EXIT_ON_ERROR(hr);
@@ -357,6 +336,92 @@ CaptureStop:
 		return hr;
 }
 
+LRESULT ProcessThread()
+{
+	//‘∂∂À”Ô“Ù ˝æ›
+	short *sFarEnd = new short[ciFrameSize];
+	FILE *fpFarWave = fopen("farEnd.wav", "rb");
+	int iFarWaveEnd = fseek(fpFarWave, 0, SEEK_END);
+	int iFarWaveBegin = fseek(fpFarWave, 44, SEEK_SET);
+	int iFarWaveLength = (iFarWaveEnd - iFarWaveBegin) / sizeof(short);
+
+	fread(sFarEnd, sizeof(short), ciFrameSize, fpFarWave);
+
+	FILE *fp_far = fopen("farEndSound.wav", "rb");
+	FILE *fp_near = fopen("mix.wav", "rb");
+	FILE *fp_out = fopen("out.wav", "wb");
+
+	short *far_frame;
+	short *near_frame;
+	short *out_frame;
+
+	far_frame = new short[44];
+	near_frame = new short[44];
+	out_frame = new short[44];
+
+	char* data;
+	data = new char[44];
+	fread(far_frame, sizeof(char), 44, fp_far);
+	long fileBegin = ftell(fp_far);
+	fwrite(far_frame, sizeof(char), 44, fp_out);
+	fread(near_frame, sizeof(char), 44, fp_near);
+
+	delete[]far_frame;
+	delete[]near_frame;
+	delete[]out_frame;
+
+	fseek(fp_far, 0, SEEK_END);
+	long fileEnd = ftell(fp_far);
+
+
+	fseek(fp_far, 44, SEEK_SET);
+	fseek(fp_near, 44, SEEK_SET);
+	int fileLength = (fileEnd - fileBegin) / 2;
+	int audioLength1s = fileLength;
+	far_frame = new short[audioLength1s];
+	near_frame = new short[audioLength1s];
+	out_frame = new short[audioLength1s];
+	for (int i = 0; i < 5; i++)
+	{
+		fread(far_frame, sizeof(short), audioLength1s, fp_far);
+		fread(near_frame, sizeof(short), audioLength1s, fp_near);
+
+		float *farEnd_f = new float[audioLength1s];
+		for (int i = 0; i < audioLength1s; i++)
+		{
+			farEnd_f[i] = far_frame[i] / float(65536 / 2);
+		}
+
+		emxArray_real32_T  *farEnd;
+		farEnd = emxCreateWrapper_real32_T(farEnd_f, audioLength1s, 1);
+
+
+		float *nearEnd_f = new float[audioLength1s];
+		for (int i = 0; i < audioLength1s; i++)
+		{
+			nearEnd_f[i] = near_frame[i] / float(65536 / 2);
+		}
+
+		emxArray_real32_T  *nearEnd;
+		nearEnd = emxCreateWrapper_real32_T(nearEnd_f, audioLength1s, 1);
+
+		int delay = delayEstimation(farEnd, nearEnd);
+
+
+		float *echo_f = new float[audioLength1s];
+
+		emxArray_real32_T *echo, *m, *en;
+		echo = emxCreateWrapper_real32_T(echo_f, audioLength1s, 1);
+		m = emxCreateWrapper_real32_T(echo_f, 20, 1);
+		en = emxCreateWrapper_real32_T(echo_f, audioLength1s, 1);
+
+		NLMS(nearEnd, farEnd, 20, 1, 0, echo, m, en);
+
+		int a = 0;
+	}
+	return S_OK;
+}
+
 int main()
 {
 	RenderDataTail = new WaveData();
@@ -378,87 +443,7 @@ int main()
 	HANDLE hCaptureThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)CaptureThread, NULL, 0, 0);
 
 	WaitForSingleObject(hRenderThread, INFINITE);
-	//‘∂∂À”Ô“Ù ˝æ›
-	//short *sFarEnd = new short[ciFrameSize];
-	//FILE *fpFarWave = fopen("farEnd.wav", "rb");
-	//int iFarWaveEnd = fseek(fpFarWave, 0, SEEK_END);
-	//int iFarWaveBegin = fseek(fpFarWave, 44, SEEK_SET);
-	//int iFarWaveLength = (iFarWaveEnd - iFarWaveBegin) / sizeof(short);
-
-	//fread(sFarEnd, sizeof(short), ciFrameSize, fpFarWave);
-
-	//FILE *fp_far = fopen("farEndSound.wav", "rb");
-	//FILE *fp_near = fopen("mix.wav", "rb");
-	//FILE *fp_out = fopen("out.wav", "wb");
-
-	//short *far_frame;
-	//short *near_frame;
-	//short *out_frame;
-
-	//far_frame = new short[44];
-	//near_frame = new short[44];
-	//out_frame = new short[44];
-
-	//char* data;
-	//data = new char[44];
-	//fread(far_frame, sizeof(char), 44, fp_far);
-	//long fileBegin = ftell(fp_far);
-	//fwrite(far_frame, sizeof(char), 44, fp_out);
-	//fread(near_frame, sizeof(char), 44, fp_near);
-
-	//delete[]far_frame;
-	//delete[]near_frame;
-	//delete[]out_frame;
-
-	//fseek(fp_far, 0, SEEK_END);
-	//long fileEnd = ftell(fp_far);
-
-
-	//fseek(fp_far, 44, SEEK_SET);
-	//fseek(fp_near, 44, SEEK_SET);
-	//int fileLength = (fileEnd - fileBegin) / 2;
-	//int audioLength1s = fileLength;
-	//far_frame = new short[audioLength1s];
-	//near_frame = new short[audioLength1s];
-	//out_frame = new short[audioLength1s];
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	fread(far_frame, sizeof(short), audioLength1s, fp_far);
-	//	fread(near_frame, sizeof(short), audioLength1s, fp_near);
-
-	//	float *farEnd_f = new float[audioLength1s];
-	//	for (int i = 0; i < audioLength1s; i++)
-	//	{
-	//		farEnd_f[i] = far_frame[i] / float(65536 / 2);
-	//	}
-
-	//	emxArray_real32_T  *farEnd;
-	//	farEnd = emxCreateWrapper_real32_T(farEnd_f, audioLength1s, 1);
-
-
-	//	float *nearEnd_f = new float[audioLength1s];
-	//	for (int i = 0; i < audioLength1s; i++)
-	//	{
-	//		nearEnd_f[i] = near_frame[i] / float(65536 / 2);
-	//	}
-
-	//	emxArray_real32_T  *nearEnd;
-	//	nearEnd = emxCreateWrapper_real32_T(nearEnd_f, audioLength1s, 1);
-
-	//	int delay = delayEstimation(farEnd, nearEnd);
-
-
-	//	float *echo_f = new float[audioLength1s];
-
-	//	emxArray_real32_T *echo, *m, *en;
-	//	echo = emxCreateWrapper_real32_T(echo_f, audioLength1s, 1);
-	//	m = emxCreateWrapper_real32_T(echo_f, 20, 1);
-	//	en = emxCreateWrapper_real32_T(echo_f, audioLength1s, 1);
-
-	//	NLMS(nearEnd, farEnd, 20, 1, 0, echo, m, en);
-
-	//	int a = 0;
-	//}
+	
 	DeleteCriticalSection(&criticalSection);
 	return 0;
 }
